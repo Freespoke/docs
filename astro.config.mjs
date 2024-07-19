c3a192001618d260ae908a7e61ea12239c5df29b
John Pedrie <john@freespoke.com>
2024-07-19 15:02:29 -0400

    init

---
+++ astro.config.mjs
@@ -0,0 +1,31 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'Freespoke',
+			social: {
+				github: 'https://github.com/Freespoke',
+			},
+			sidebar: [
+				'get-started',
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+			customCss: ['./src/tailwind.css'],
+		}),
+		tailwind({ applyBaseStyles: false }),
+	],
+});
---
+++ src/content/docs/premium/about-premium.md
@@ -0,0 +1,4 @@
+---
+title: About Freespoke Premium
+description: An introduction to Freespoke Premium's features and benefits.
+---
