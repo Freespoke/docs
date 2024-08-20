3955ed7082ca3dbb6edc1b8ec146f191ef870f5c
davem0n <david.montanez10@gmail.com>
2024-08-20 10:27:27 -0500

    [FRE-5596] Customize Docs.freespoke.com Homepage (#2)
    * remove extra content on homepage, add Freespoke torch logo to title
    
    * add freespoke favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import tailwind from '@astrojs/tailwind';
+import freespoke_torch from "./src/assets/freespoke_torch.svg"
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,6 +9,10 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'Freespoke',
+			logo: {
+				dark: freespoke_torch,
+				light: freespoke_torch,
+			},
 			social: {
 				github: 'https://github.com/Freespoke',
 			},
@@ -25,6 +30,18 @@ export default defineConfig({
 					label: 'Privacy',
 					autogenerate: { directory: 'privacy' },
 				}
+			],
+			favicon: '/favicon-16x16.png',
+			head: [
+				// Add ICO favicon fallback for Safari.
+				{
+				tag: 'link',
+				attrs: {
+					rel: 'icon',
+					href: '/public/favicon.ico',
+					sizes: '32x32',
+				},
+				},
 			],
 			customCss: ['./src/tailwind.css', './src/styles/custom.css'],
 		}),
