6b6f77cab254e095696613da6b1386ad0afdcc55
John Pedrie <john@freespoke.com>
2024-09-09 16:15:03 -0400

    add premium signup page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,54 +1,56 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import tailwind from '@astrojs/tailwind';
-import freespoke_torch from "./src/assets/freespoke_torch.svg"
+import freespoke_torch from "./src/assets/freespoke_torch.svg";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://docs.freespoke.com",
-	integrations: [
-		starlight({
-			title: 'Freespoke',
-			logo: {
-				dark: freespoke_torch,
-				light: freespoke_torch,
-			},
-			social: {
-				github: 'https://github.com/Freespoke',
-			},
-			sidebar: [
-				'get-started',
-				{
-					label: 'About Freespoke',
-					autogenerate: { directory: 'about' },
-				},
-				{
-					label: 'Freespoke Accounts',
-					autogenerate: { directory: 'account' },
-				},
-				{
-					label: 'Freespoke Premium',
-					autogenerate: { directory: 'premium' },
-				},
-				{
-					label: 'Privacy',
-					autogenerate: { directory: 'privacy' },
-				}
-			],
-			favicon: '/favicon-16x16.png',
-			head: [
-				// Add ICO favicon fallback for Safari.
-				{
-				tag: 'link',
-				attrs: {
-					rel: 'icon',
-					href: '/public/favicon.ico',
-					sizes: '32x32',
-				},
-				},
-			],
-			customCss: ['./src/tailwind.css', './src/styles/custom.css'],
-		}),
-		tailwind({ applyBaseStyles: false }),
-	],
+  site: "https://docs.freespoke.com",
+  integrations: [starlight({
+    title: 'Freespoke',
+    logo: {
+      dark: freespoke_torch,
+      light: freespoke_torch
+    },
+    social: {
+      github: 'https://github.com/Freespoke'
+    },
+    sidebar: ['get-started', {
+      label: 'About Freespoke',
+      autogenerate: {
+        directory: 'about'
+      }
+    }, {
+      label: 'Freespoke Accounts',
+      autogenerate: {
+        directory: 'account'
+      }
+    }, {
+      label: 'Freespoke Premium',
+      autogenerate: {
+        directory: 'premium'
+      }
+    }, {
+      label: 'Privacy',
+      autogenerate: {
+        directory: 'privacy'
+      }
+    }],
+    favicon: '/favicon-16x16.png',
+    head: [
+    // Add ICO favicon fallback for Safari.
+    {
+      tag: 'link',
+      attrs: {
+        rel: 'icon',
+        href: '/public/favicon.ico',
+        sizes: '32x32'
+      }
+    }],
+    customCss: ['./src/tailwind.css', './src/styles/custom.css']
+  }), tailwind({
+    applyBaseStyles: false
+  }), icon()]
 });
--- src/content/docs/premium/overview.md
+++
@@ -1,6 +0,0 @@
----
-title: Overview
-description: An introduction to Freespoke Premium's features and benefits.
-sidebar:
-    order: 1
----
